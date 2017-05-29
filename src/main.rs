190839f772e7ea2a200d8771e52bd77abee6ec43
Kevin Traver <kevintraver@gmail.com>
2017-05-29 15:24:39 -0600

    Read fuzzy map from yml file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 extern crate chrono;
+extern crate serde_derive;
+extern crate serde_yaml;
+
+use std::io;
+use std::fs::File;
+use std::io::prelude::*;
 use chrono::prelude::*;
 use std::collections::BTreeMap;
 
@@ -10,7 +16,7 @@ fn main() {
 }
 
 fn current_title(state: u32, now: DateTime<Local>) -> String {
-    let fuzzy_map = generate_fuzzy_map();
+    let fuzzy_map = read_fuzzy_map().unwrap();
 
     // Time descriptions may refer to the current or the following hour
     let mut hour_offset: u32 = 0;
@@ -63,56 +69,10 @@ fn get_state(now: DateTime<Local>) -> u32 {
 
 }
 
-fn generate_fuzzy_map() -> BTreeMap<String, String> {
-    let mut fuzzy_map = BTreeMap::new();
-    fuzzy_map.insert("S00".to_string(), "{} o’clock".to_string());
-    fuzzy_map.insert("S01".to_string(), "shortly after {}".to_string());
-    fuzzy_map.insert("S02".to_string(), "five past {}".to_string());
-    fuzzy_map.insert("S03".to_string(), "ten past {}".to_string());
-    fuzzy_map.insert("S04".to_string(), "quarter past {}".to_string());
-    fuzzy_map.insert("S05".to_string(), "twenty past {}".to_string());
-    fuzzy_map.insert("S06".to_string(), "twentyfive past {}".to_string());
-    fuzzy_map.insert("S07".to_string(), "half past {}".to_string());
-    fuzzy_map.insert("S08".to_string(), "twentyfive to {}".to_string());
-    fuzzy_map.insert("S09".to_string(), "twenty to {}".to_string());
-    fuzzy_map.insert("S10".to_string(), "quarter to {}".to_string());
-    fuzzy_map.insert("S11".to_string(), "ten to {}".to_string());
-    fuzzy_map.insert("S12".to_string(), "five to {}".to_string());
-    fuzzy_map.insert("S13".to_string(), "nearly {}".to_string());
-    fuzzy_map.insert("S00h".to_string(), "current".to_string());
-    fuzzy_map.insert("S01h".to_string(), "current".to_string());
-    fuzzy_map.insert("S02h".to_string(), "current".to_string());
-    fuzzy_map.insert("S03h".to_string(), "current".to_string());
-    fuzzy_map.insert("S04h".to_string(), "current".to_string());
-    fuzzy_map.insert("S05h".to_string(), "current".to_string());
-    fuzzy_map.insert("S06h".to_string(), "current".to_string());
-    fuzzy_map.insert("S07h".to_string(), "current".to_string());
-    fuzzy_map.insert("S08h".to_string(), "next".to_string());
-    fuzzy_map.insert("S09h".to_string(), "next".to_string());
-    fuzzy_map.insert("S10h".to_string(), "next".to_string());
-    fuzzy_map.insert("S11h".to_string(), "next".to_string());
-    fuzzy_map.insert("S12h".to_string(), "next".to_string());
-    fuzzy_map.insert("S13h".to_string(), "next".to_string());
-    fuzzy_map.insert("H00".to_string(), "twelve".to_string());
-    fuzzy_map.insert("H01".to_string(), "one".to_string());
-    fuzzy_map.insert("H02".to_string(), "two".to_string());
-    fuzzy_map.insert("H03".to_string(), "three".to_string());
-    fuzzy_map.insert("H04".to_string(), "four".to_string());
-    fuzzy_map.insert("H05".to_string(), "five".to_string());
-    fuzzy_map.insert("H06".to_string(), "six".to_string());
-    fuzzy_map.insert("H07".to_string(), "seven".to_string());
-    fuzzy_map.insert("H08".to_string(), "eight".to_string());
-    fuzzy_map.insert("H09".to_string(), "nine".to_string());
-    fuzzy_map.insert("H10".to_string(), "ten".to_string());
-    fuzzy_map.insert("H11".to_string(), "eleven".to_string());
-    fuzzy_map.insert("M00".to_string(), "midnight".to_string());
-    fuzzy_map.insert("M12".to_string(), "noon".to_string());
-    fuzzy_map.insert("D00".to_string(), "sunday".to_string());
-    fuzzy_map.insert("D01".to_string(), "monday".to_string());
-    fuzzy_map.insert("D02".to_string(), "tuesday".to_string());
-    fuzzy_map.insert("D03".to_string(), "wednesday".to_string());
-    fuzzy_map.insert("D04".to_string(), "thursday".to_string());
-    fuzzy_map.insert("D05".to_string(), "friday".to_string());
-    fuzzy_map.insert("D06".to_string(), "saturday".to_string());
-    fuzzy_map
+fn read_fuzzy_map() -> io::Result<BTreeMap<String, String>> {
+    let mut file = File::open("fuzzy_map.yml")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let fuzzy_map: BTreeMap<String, String> = serde_yaml::from_str(&contents).unwrap();
+    Ok(fuzzy_map)
 }
